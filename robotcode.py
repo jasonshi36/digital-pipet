ab69e70dd1a8c695c5c90e11b17e63f234da261e
jasonshi36 <166175454+jasonshi36@users.noreply.github.com>
2024-05-09 16:52:51 -0500

    Update robotcode.py
    Code for operate the pipette robot
    has interface allow user choose wells/volume/rounds/waittime

--- robotcode.py
+++ robotcode.py
@@ -1,128 +1,127 @@
 from pymycobot.mycobot import MyCobot
 import serial
+import tkinter as tk
 import time
 
 # 初始化 MyCobot
 mc = MyCobot('/dev/ttyAMA0', 1000000)
 
-# 打开液体泵的串口
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 time.sleep(1)
-print(ser.readline())
 
 angles_list = {
     'ini': [0, 0, 0, 0, 0, 0],
     'TFA_up': [26,(-20),(-35),76,(-17),(0)],
     'TFA': [27.07,(-58.5),(-35),90,(-38.19),(-1.5)],
     'plate_up': [-60, 0, -41, 70, 0, 0],
-    'A1': [(-57.3),(-22),(-36.5),64,(-56.5),0],
-    'B1': [(-54.8),(-22),(-36.5),64,(-56.2),0],
-    'C1': [(-53.5),(-22),(-36.5),64,(-54.5),0],
-    'D1': [(-51),(-22),(-36.5),64,(-53.5),0],
-    'E1': [(-49.2),(-22),(-36.5),64,(-52.2),0],
-    'F1': [(-50.5),(-22),(-36.5),64,(-45.5),0],
-    'G1': [(-49),(-22),(-36.5),64,(-40.5),0],
-    'H1': [(-50.5),(-22),(-36.5),64,(-30),0],
+    'A1': [(-57.3),(-19),(-36.5),64,(-56.5),0],
+    'B1': [(-54.8),(-19),(-36.5),64,(-56.2),0],
+    'C1': [(-53.5),(-19),(-36.5),64,(-54.5),0],
+    'D1': [(-51),(-19),(-36.5),64,(-53.5),0],
+    'E1': [(-49.2),(-19),(-36.5),64,(-52.2),0],
+    'F1': [(-47.5),(-19),(-36.5),64,(-49),0],
+    'G1': [(-49),(-19),(-36.5),64,(-40.5),0],
+    'H1': [(-50.5),(-19),(-36.5),64,(-30),0],
     
-    'A2': [(-54),(-21),(-36.5),64,(-69.3),0],
-    'B2': [(-51),(-21),(-36.5),64,(-66.8),0],
-    'C2': [(-48.4),(-21),(-36.5),64,(-69.5),0],
-    'D2': [(-46.4),(-21),(-36.5),64,(-66.3),0],
-    'E2': [(-44),(-21),(-36.5),64,(-68),0],
-    'F2': [(-42.5),(-21),(-36.5),64,(-66),0],
-    'G2': [(-41),(-21),(-36.5),64,(-62.5),0],
-    'H2': [(-41),(-21),(-36.5),64,(-57),0],
+    'A2': [(-54),(-19),(-36.5),64,(-69.8),0],
+    'B2': [(-51),(-19),(-36.5),64,(-68.8),0],
+    'C2': [(-48.4),(-19),(-36.5),64,(-70.5),0],
+    'D2': [(-46.4),(-19),(-36.5),64,(-66.9),0],
+    'E2': [(-44),(-19),(-36.5),64,(-68),0],
+    'F2': [(-42.5),(-19),(-36.5),64,(-66.6),0],
+    'G2': [(-41),(-19),(-36.5),64,(-63.3),0],
+    'H2': [(-40.5),(-19),(-36.5),64,(-57),0],
     
-    'A3': [(-50.7),(-20),(-36.5),62,(-77.5),0],
-    'B3': [(-47.7),(-20),(-36.5),62,(-74.5),0],
-    'C3': [(-45.5),(-20),(-36.5),62,(-74),0],
-    'D3': [(-42.8),(-20),(-36.5),62,(-73),0],
-    'E3': [(-41),(-20),(-36.5),62,(-72),0],
-    'F3': [(-39.5),(-20),(-36.5),62,(-69.5),0],
-    'G3': [(-37),(-20),(-36.5),62,(-69),0],
-    'H3': [(-35.3),(-20),(-36.5),62,(-68),0],
+    'A3': [(-50.7),(-19),(-36.5),62,(-77.5),0],
+    'B3': [(-47.7),(-19),(-36.5),62,(-74.5),0],
+    'C3': [(-45.5),(-19),(-36.5),62,(-74),0],
+    'D3': [(-42.8),(-19),(-36.5),62,(-74),0],
+    'E3': [(-41),(-19),(-36.5),62,(-72),0],
+    'F3': [(-39.5),(-19),(-36.5),62,(-69.5),0],
+    'G3': [(-37),(-19),(-36.5),62,(-69),0],
+    'H3': [(-35.3),(-19),(-36.5),62,(-67),0],
     
-    'A4': [(-48),(-20),(-36.5),62,(-85.5),0],
-    'B4': [(-43.8),(-20),(-36.5),62,(-87),0],
-    'C4': [(-41.5),(-20),(-36.5),62,(-86),0],
-    'D4': [(-39.2),(-20),(-36.5),62,(-85.5),0],
-    'E4': [(-37),(-20),(-36.5),62,(-85),0],
-    'F4': [(-34.5),(-20),(-36.5),62,(-84.5),0],
-    'G4': [(-33.2),(-20),(-36.5),62,(-81),0],
-    'H4': [(-30.5),(-20),(-36.5),62,(-81),0],
+    'A4': [(-48),(-19),(-36.5),62,(-88.5),0],
+    'B4': [(-44.5),(-19),(-36.5),62,(-88.5),0],
+    'C4': [(-42),(-19),(-36.5),62,(-87.5),0],
+    'D4': [(-39.2),(-19),(-36.5),62,(-85.5),0],
+    'E4': [(-37.5),(-19),(-36.5),62,(-85),0],
+    'F4': [(-34.5),(-19),(-36.5),62,(-84.5),0],
+    'G4': [(-33.2),(-19),(-36.5),62,(-81),0],
+    'H4': [(-30.5),(-19),(-36.5),62,(-81),0],
     
-    'A5': [(-45),(-19),(-36.5),62,(-97),0],
-    'B5': [(-41),(-19),(-36.5),62,(-97),0],
-    'C5': [(-37.7),(-19),(-36.5),62,(-98.5),0],
-    'D5': [(-36),(-19),(-36.5),62,(-95),0],
+    'A5': [(-45),(-19),(-36.5),62,(-99),0],
+    'B5': [(-41.5),(-19),(-36.5),62,(-98),0],
+    'C5': [(-38.5),(-19),(-36.5),62,(-98.5),0],
+    'D5': [(-36.5),(-19),(-36.5),62,(-95),0],
     'E5': [(-33.4),(-19),(-36.5),62,(-96.5),0],
     'F5': [(-31.5),(-19),(-36.5),62,(-93),0],
-    'G5': [(-29),(-19),(-36.5),62,(-92),0],
+    'G5': [(-29.3),(-19),(-36.5),62,(-92),0],
     'H5': [(-27.5),(-19),(-36.5),62,(-89.3),0],
     
-    'A6': [(-42.5),(-19),(-36.5),62,(-106),0],
-    'B6': [(-39.5),(-19),(-36.5),62,(-105),0],
-    'C6': [(-36),(-19),(-36.5),62,(-105),0],
+    'A6': [(-43),(-19),(-36.5),62,(-109.5),0],
+    'B6': [(-39.5),(-19),(-36.5),62,(-108),0],
+    'C6': [(-36),(-19),(-36.5),62,(-108),0],
     'D6': [(-33.3),(-19),(-36.5),62,(-105),0],
-    'E6': [(-31.5),(-19),(-36.5),62,(-106),0],
+    'E6': [(-31.5),(-19),(-36.5),62,(-102),0],
     'F6': [(-29),(-19),(-36.5),62,(-103),0],
     'G6': [(-27),(-19),(-36.5),62,(-102),0],
-    'H6': [(-25.5),(-19),(-36.5),62,(-97.5),0],
+    'H6': [(-25),(-19),(-36.5),62,(-99),0],
     
-    'A7': [(-40.8),(-19),(-36.5),62,(-114),0],
-    'B7': [(-37.3),(-19),(-36.5),62,(-112.5),0],
-    'C7': [(-33.8),(-19),(-36.5),62,(-113.5),0],
-    'D7': [(-31.3),(-19),(-36.5),62,(-112),0],
-    'E7': [(-28.5),(-19),(-36.5),62,(-111.5),0],
+    'A7': [(-41.5),(-19),(-36.5),62,(-116),0],
+    'B7': [(-37.8),(-19),(-36.5),62,(-114),0],
+    'C7': [(-34.3),(-19),(-36.5),62,(-114),0],
+    'D7': [(-31.8),(-19),(-36.5),62,(-113),0],
+    'E7': [(-28.5),(-19),(-36.5),62,(-112.5),0],
     'F7': [(-26.8),(-19),(-36.5),62,(-110),0],
     'G7': [(-23.8),(-19),(-36.5),62,(-110),0],
     'H7': [(-21.5),(-19),(-36.5),62,(-106.4),0],
     
-    'A8': [(-39.5),(-18.5),(-36.5),62,(-122),0],
-    'B8': [(-36),(-18.5),(-36.5),62,(-121.5),0],
-    'C8': [(-32.5),(-18.5),(-36.5),62,(-121.5),0],
-    'D8': [(-30),(-18.5),(-36.5),62,(-119),0],
-    'E8': [(-26.5),(-18.5),(-36.5),62,(-119.5),0],
-    'F8': [(-23.5),(-18.5),(-36.5),62,(-119.5),0],
-    'G8': [(-21),(-18.5),(-36.5),62,(-117),0],
-    'H8': [(-18.5),(-18.5),(-36.5),62,(-115),0],
+    'A8': [(-39.5),(-18.5),(-36.5),62,(-125),0],
+    'B8': [(-36),(-18.5),(-36.5),62,(-126),0],
+    'C8': [(-32.5),(-18.5),(-36.5),62,(-126),0],
+    'D8': [(-30),(-18.5),(-36.5),62,(-124),0],
+    'E8': [(-26.5),(-18.5),(-36.5),62,(-122),0],
+    'F8': [(-23.8),(-18.5),(-36.5),62,(-119),0],
+    'G8': [(-21.2),(-18.5),(-36.5),62,(-121),0],
+    'H8': [(-18.8),(-18.5),(-36.5),62,(-115),0],
    
     
-    'A9': [(-38.5),(-20),(-36.5),62,(-127.5),0],
-    'B9': [(-35),(-20),(-36.5),62,(-127.5),0],
-    'C9': [(-31),(-20),(-36.5),62,(-127),0],
-    'D9': [(-28),(-20),(-36.5),62,(-126),0],
+    'A9': [(-39),(-20),(-36.5),62,(-133),0],
+    'B9': [(-35),(-20),(-36.5),62,(-130),0],
+    'C9': [(-31),(-20),(-36.5),62,(-129.5),0],
+    'D9': [(-28),(-20),(-36.5),62,(-129),0],
     'E9': [(-25),(-20),(-36.5),62,(-126),0],
-    'F9': [(-22),(-20),(-36.5),62,(-125),0],
-    'G9': [(-19),(-20),(-36.5),62,(-124),0],
-    'H9': [(-16.5),(-20),(-36.5),62,(-121),0],
+    'F9': [(-22),(-20),(-36.5),62,(-126),0],
+    'G9': [(-19),(-20),(-36.5),62,(-124.5),0],
+    'H9': [(-16.5),(-20),(-36.5),62,(-122.5),0],
     
-    'A10': [(-38),(-20),(-36.5),62,(-136),0],
-    'B10': [(-34),(-20),(-36.5),62,(-134.5),0],
-    'C10': [(-30),(-20),(-36.5),62,(-134),0],
-    'D10': [(-26.3),(-20),(-36.5),62,(-133.5),0],
-    'E10': [(-23),(-20),(-36.5),62,(-132),0],
-    'F10': [(-20),(-20),(-36.5),62,(-131),0],
-    'G10': [(-17),(-20),(-36.5),62,(-130),0],
-    'H10': [(-14),(-20),(-36.5),62,(-127),0],
+    'A10': [(-39),(-20),(-36.5),62,(-139),0],
+    'B10': [(-35),(-20),(-36.5),62,(-137.5),0],
+    'C10': [(-31),(-20),(-36.5),62,(-137),0],
+    'D10': [(-27.3),(-20),(-36.5),62,(-137.5),0],
+    'E10': [(-23.5),(-20),(-36.5),62,(-135.5),0],
+    'F10': [(-20),(-20),(-36.5),62,(-135),0],
+    'G10': [(-17),(-20),(-36.5),62,(-131),0],
+    'H10': [(-14),(-20),(-36.5),62,(-129.5),0],
     
-    'A11': [(-38),(-20),(-36.5),62,(-142),0],
-    'B11': [(-33.5),(-20),(-36.5),62,(-142),0],
-    'C11': [(-29.5),(-20),(-36.5),62,(-141),0],
-    'D11': [(-25.5),(-20),(-36.5),62,(-140),0],
-    'E11': [(-21.8),(-20),(-36.5),62,(-138.5),0],
-    'F11': [(-18.3),(-20),(-36.5),62,(-137.5),0],
-    'G11': [(-15.8),(-20),(-36.5),62,(-136),0],
-    'H11': [(-12.5),(-20),(-36.5),62,(-134.5),0],
+    'A11': [(-39.5),(-19),(-36.5),62,(-146),0],
+    'B11': [(-35),(-19),(-36.5),62,(-146),0],
+    'C11': [(-30.8),(-19),(-36.5),62,(-145),0],
+    'D11': [(-26.5),(-19),(-36.5),62,(-145),0],
+    'E11': [(-22.4),(-19),(-36.5),62,(-142),0],
+    'F11': [(-19.5),(-19),(-36.5),62,(-140),0],
+    'G11': [(-15.8),(-19),(-36.5),62,(-138),0],
+    'H11': [(-12.5),(-19),(-36.5),62,(-136.5),0],
     
-    'A12': [(-38.5),(-20),(-36.5),62,(-148),0],
-    'B12': [(-33),(-20),(-36.5),62,(-147.5),0],
-    'C12': [(-29),(-20),(-36.5),62,(-146.5),0],
-    'D12': [(-25.5),(-20),(-36.5),62,(-146),0],
-    'E12': [(-21.5),(-20),(-36.5),62,(-145),0],
-    'F12': [(-17),(-20),(-36.5),62,(-143),0],
-    'G12': [(-13.5),(-20),(-36.5),62,(-142),0],
-    'H12': [(-10),(-20),(-36.5),62,(-139),0]
+    'A12': [(-39),(-19),(-36.5),62,(-151),0],
+    'B12': [(-33.5),(-19),(-36.5),62,(-150.5),0],
+    'C12': [(-29.5),(-19),(-36.5),62,(-149.5),0],
+    'D12': [(-25.8),(-19),(-36.5),62,(-149),0],
+    'E12': [(-21.8),(-19),(-36.5),62,(-148),0],
+    'F12': [(-17.5),(-19),(-36.5),62,(-146),0],
+    'G12': [(-14),(-19),(-36.5),62,(-145),0],
+    'H12': [(-10.4),(-19),(-36.5),62,(-142),0]
     
     # More coords need to be added as A2, A3...
 }
@@ -134,7 +133,7 @@ len_5mL = 1528
 len_10mL = 1286
 
 volume_list = {
-
+   '150uL' : 7,
    '300uL' : 12,
    '500uL' :30,
    '1mL' : 55
@@ -206,126 +205,133 @@ def refill_pump():
     move_mycobot(angles_list['ini'], 20, 3)
     move_mycobot(angles_list['plate_up'], 20, 3)
     
+
+
+
+# 创建主窗口
+root = tk.Tk()
+root.title("Liquid Transfer System")
+
+interval_label = tk.Label(root, text="Interval between rounds (minutes):")
+interval_label.grid(row=14, column=0, padx=(10, 0), pady=5, sticky="e")
+
+interval_entry = tk.Entry(root)
+interval_entry.grid(row=14, column=1, padx=(0, 10), pady=5)
+
+rounds_label = tk.Label(root, text="Number of rounds:")
+rounds_label.grid(row=15, column=0, padx=(10, 0), pady=5, sticky="e")
+
+rounds_entry = tk.Entry(root)
+rounds_entry.grid(row=15, column=1, padx=(0, 10), pady=5)
+# 函数：全选所有选项
+def select_all():
+    for _, var in hole_checkboxes:
+        var.set(True)
+
+# 函数：取消全选所有选项
+def deselect_all():
+    for _, var in hole_checkboxes:
+        var.set(False)
+# Function to select all checkboxes in a column
+def select_column(column_index):
+    for i in range(column_index * 8, (column_index + 1) * 8):
+        hole_checkboxes[i][1].set(True)
+
+# Function to deselect all checkboxes in a column
+def deselect_column(column_index):
+    for i in range(column_index * 8, (column_index + 1) * 8):
+        hole_checkboxes[i][1].set(False)
+        
+# 添加全选和取消全选按钮
+select_all_button = tk.Button(root, text="Select All", command=select_all)
+select_all_button.grid(row=3, column=1, padx=5, pady=5)
+
+deselect_all_button = tk.Button(root, text="Deselect All", command=deselect_all)
+deselect_all_button.grid(row=4, column=1, padx=5, pady=5)
+
+# 用于存储选中的孔位
+selected_holes = []
+
+
+# 添加按钮来完成液体转移功能
+def transfer_liquid():
+    selected_volume = volume_selection.get()
+
+    if selected_volume != "Select Volume" and selected_holes:
+        interval_minutes = int(interval_entry.get()) if interval_entry.get() else 0
+        interval_seconds = interval_minutes * 60
+        num_rounds = int(rounds_entry.get()) if rounds_entry.get() else 1
+
+        for i in range(num_rounds):
+            is_last_round = (i == num_rounds - 1)  # 检查是否是最后一轮
+            for hole in selected_holes:
+                dispense_liquid(selected_volume, hole)
+                print(f"Transferring {selected_volume} to {hole}")
+            empty_pump()
+            wait_interval(interval_seconds, is_last_round)  # 等待时间处理改为传递 is_last_round
+    else:
+        print("Please select at least one hole and a volume.")
+
+# Function to handle waiting period between rounds
+def wait_interval(interval_seconds, is_last_round):
+    if not is_last_round:
+        print(f"Waiting for {interval_seconds // 60} minutes between rounds.")
+        time.sleep(interval_seconds)
+    else:
+        print("Transfer finish")
+
+# 创建96个复选框模拟96孔板
+hole_checkboxes = []
+for i in range(12):
+    column_frame = tk.Frame(root)
+    column_frame.grid(row=0, column=(i+2), padx=5, pady=5)
+    for j in range(8):
+        hole = f"{chr(65+j)}{i+1}"  # 构建孔位标识，例如 A1, B1, ..., H12
+        var = tk.BooleanVar()
+        checkbox = tk.Checkbutton(column_frame, text=hole, variable=var)
+        checkbox.pack(side=tk.TOP, padx=5)
+        hole_checkboxes.append((hole, var))
+
+    # Add Check All and Uncheck All buttons for each column
+    uncheck_button = tk.Button(column_frame, text="-", command=lambda col=i: deselect_column(col))
+    uncheck_button.pack(side=tk.BOTTOM, padx=5)
+    check_button = tk.Button(column_frame, text="+", command=lambda col=i: select_column(col))
+    check_button.pack(side=tk.BOTTOM, padx=5)
     
-    # Add your code here to refill the pump with 10ml liquid
+    
 
-# 测试运行
-try:
-   
+# 添加选择液体体积的下拉菜单
+volume_selection = tk.StringVar()
+volume_selection.set("Select Volume")
 
-   dispense_liquid('1mL', 'A1')
-   dispense_liquid('300uL', 'B1')
-   dispense_liquid('1mL', 'C1')
-   dispense_liquid('300uL', 'D1')
-   dispense_liquid('1mL', 'E1')
-   dispense_liquid('1mL', 'F1')
-   dispense_liquid('300uL', 'G1')
-   dispense_liquid('1mL', 'H1')
-   
-   dispense_liquid('300uL', 'A2')
-   dispense_liquid('1mL', 'B2')
-   dispense_liquid('300uL', 'C2')
-   dispense_liquid('1mL', 'D2')
-   dispense_liquid('300uL', 'E2')
-   dispense_liquid('1mL', 'F2')
-   dispense_liquid('300uL', 'G2')
-   dispense_liquid('1mL', 'H2')
-   '''
-   dispense_liquid('300uL', 'A3')
-   dispense_liquid('300uL', 'B3')
-   dispense_liquid('300uL', 'C3')
-   dispense_liquid('300uL', 'D3')
-   dispense_liquid('300uL', 'E3')
-   dispense_liquid('300uL', 'F3')
-   dispense_liquid('300uL', 'G3')
-   dispense_liquid('300uL', 'H3')
-   '''
-   dispense_liquid('1mL', 'A4')
-   dispense_liquid('300uL', 'B4')
-   dispense_liquid('1mL', 'C4')
-   dispense_liquid('300uL', 'D4')
-   dispense_liquid('1mL', 'E4')
-   dispense_liquid('300uL', 'F4')
-   dispense_liquid('1mL', 'G4')
-   dispense_liquid('300uL', 'H4')
-   '''
-   
-   dispense_liquid('300uL', 'A5')
-   dispense_liquid('300uL', 'B5')
-   dispense_liquid('300uL', 'C5')
-   dispense_liquid('300uL', 'D5')
-   dispense_liquid('300uL', 'E5')
-   dispense_liquid('300uL', 'F5')
-   dispense_liquid('300uL', 'G5')
-   dispense_liquid('300uL', 'H5')
-   '''
-   
-   dispense_liquid('1mL', 'A6')
-   dispense_liquid('1mL', 'B6')
-   dispense_liquid('1mL', 'C6')
-   dispense_liquid('1mL', 'D6')
-   dispense_liquid('1mL', 'E6')
-   dispense_liquid('1mL', 'F6')
-   dispense_liquid('1mL', 'G6')
-   dispense_liquid('1mL', 'H6')
-   
-   dispense_liquid('1mL', 'A7')
-   dispense_liquid('1mL', 'B7')
-   dispense_liquid('1mL', 'C7')
-   dispense_liquid('1mL', 'D7')
-   dispense_liquid('1mL', 'E7')
-   dispense_liquid('1mL', 'F7')
-   dispense_liquid('1mL', 'G7')
-   dispense_liquid('1mL', 'H7')
-  
-   dispense_liquid('1mL', 'A8')
-   dispense_liquid('1mL', 'B8')
-   dispense_liquid('1mL', 'C8')
-   dispense_liquid('1mL', 'D8')
-   dispense_liquid('1mL', 'E8')
-   dispense_liquid('1mL', 'F8')
-   dispense_liquid('1mL', 'G8')
-   dispense_liquid('1mL', 'H8')
-   '''
-   dispense_liquid('300uL', 'A9')
-   dispense_liquid('300uL', 'B9')
-   dispense_liquid('300uL', 'C9')
-   dispense_liquid('300uL', 'D9')
-   dispense_liquid('300uL', 'E9')
-   dispense_liquid('300uL', 'F9')
-   dispense_liquid('300uL', 'G9')
-   dispense_liquid('300uL', 'H9')
-   
-   dispense_liquid('300uL', 'A10')
-   dispense_liquid('300uL', 'B10')
-   dispense_liquid('300uL', 'C10')
-   dispense_liquid('300uL', 'D10')
-   dispense_liquid('300uL', 'E10')
-   dispense_liquid('300uL', 'F10')
-   dispense_liquid('300uL', 'G10')
-   dispense_liquid('300uL', 'H10')
-   
-   dispense_liquid('300uL', 'A11')
-   dispense_liquid('300uL', 'B11')
-   dispense_liquid('300uL', 'C11')
-   dispense_liquid('300uL', 'D11')
-   dispense_liquid('300uL', 'E11')
-   dispense_liquid('300uL', 'F11')
-   dispense_liquid('300uL', 'G11')
-   dispense_liquid('300uL', 'H11')
-   
-   dispense_liquid('300uL', 'A12')
-   dispense_liquid('300uL', 'B12')
-   dispense_liquid('300uL', 'C12')
-   dispense_liquid('300uL', 'D12')
-   dispense_liquid('300uL', 'E12')
-   dispense_liquid('300uL', 'F12')
-   dispense_liquid('300uL', 'G12')
-   dispense_liquid('300uL', 'H12')
-   '''
-   empty_pump()
+volume_frame = tk.Frame(root)
+volume_frame.grid(row=12, column=0, padx=10, pady=10)
 
-finally:
-    # Close the serial ports
-    ser.close()
+tk.Label(volume_frame, text="Select Volume:").grid(row=0, column=0, padx=10)
+
+volumes = list(volume_list.keys())  # 液体体积列表
+volume_dropdown = tk.OptionMenu(volume_frame, volume_selection, *volumes)
+volume_dropdown.grid(row=0, column=1, padx=10)
+
+# 添加液体转移按钮
+transfer_button = tk.Button(root, text="Liquid Transfer", command=transfer_liquid)
+transfer_button.grid(row=13, columnspan=1, pady=5)
+
+# 在复选框状态改变时更新选中的孔位列表
+def update_selected_holes():
+    selected_holes.clear()
+    for hole, var in hole_checkboxes:
+        if var.get():
+            selected_holes.append(hole)
+
+# 绑定复选框状态变化事件
+for _, var in hole_checkboxes:
+    var.trace("w", lambda *args, var=var: update_selected_holes())
+
+# 运行主循环
+root.mainloop()
+
+
+
+
 
